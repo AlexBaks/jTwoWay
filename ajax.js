f006afd01f3f5c8a7280731687ab26a7f49e8312
Suroviy <Syroviy@bk.ru>
2014-02-05 13:48:51 +0400

    ajax.js

---
+++ ajax.js
@@ -0,0 +1,16 @@
+﻿	
+	
+	$.ajax({
+            type: "POST",
+            url: "assets/components/modxsite/connectors/mail.php",
+            data: { 
+                name: $('input[name=name]').val()
+                ,mobil: $('input[name=mobil]').val()
+                ,mail: $('input[name=mail]').val()
+                ,text: $('.formText_alex').val()
+                ,city: $('input[name=city]').val() 
+            }
+        }).done(function( msg ) {
+            $('#bottom_left').html('<br /><br /><br /><h2></h2>');
+            return false;
+        });
